d222518f125fa90c22d8100205a2c634455b84c2
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-15 19:28:00 -0300

    Criação do app e Settings
    Configurado o Settings pra reconhecer o app.

---
+++ gnc-dev/src/apps/gerenciaDev/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GerenciadevConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gerenciaDev'
--- gnc-dev/src/gnc_dev/settings.py
+++ gnc-dev/src/gnc_dev/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'apps.gerenciaDev.apps.GereciadevConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'gnc_dev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "OPTIONS": {
+            "read_default_file": BASE_DIR / "my_pgs.cnf",
+        },
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
