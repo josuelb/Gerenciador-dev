430e2cb979f75a7fbc2611eee3031b9bddeadca4
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-16 12:14:27 -0300

    Implementação do admin

--- gnc-dev/src/apps/gerenciaDev/admin.py
+++ gnc-dev/src/apps/gerenciaDev/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from models import (
+    Alocacoes,
+    Programador,
+    Projetos, 
+    Tecnologias
+)
+
 # Register your models here.
+
+admin.register(Alocacoes)
+admin.register(Programador)
+admin.register(Projetos)
+admin.register(Tecnologias)
