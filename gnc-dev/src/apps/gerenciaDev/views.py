da1f53127001827dd9ad7fe3f130b7a5dae880f8
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-16 12:12:39 -0300

    Criação base do views

--- gnc-dev/src/apps/gerenciaDev/views.py
+++ gnc-dev/src/apps/gerenciaDev/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
 
-# Create your views here.
+from gerenciaDev import serializer
+from gerenciaDev import models
+
+class ProgramadorView(viewsets.ModelViewSet):
+    serializer_class = serializer.ProgramadorSerializer
+    queryset = models.Programador.objects.all()
+
+
+class ProjetoView(viewsets.ModelViewSet):
+    serializer_class = serializer.ProjetoSerializer
+    queryset = models.Projetos.objects.all()
+
+
+class AlocacoesView(viewsets.ModelViewSet):
+    serializer_class = serializer.AlocacoeSerializer
+    queryset = models.Alocacoes.objects.all()
+
+
+class TecnologiasView(viewsets.ModelViewSet):
+    serializer_class = serializer.TecnologiaSerializer
+    queryset = models.Tecnologias.objects.all()
+
