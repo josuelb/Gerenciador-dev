aa5d256d6a511e96edd0fe09ee4fc7435d8c99c1
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-16 11:41:01 -0300

    criação do serializer

--- gnc-dev/src/apps/gerenciaDev/serializer.py
+++ gnc-dev/src/apps/gerenciaDev/serializer.py
@@ -1,3 +1,27 @@
 from rest_framework import serializers
 
 from gerenciaDev import models
+
+
+class ProgramadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Programador
+        fields = "__all__"
+    
+
+class ProjetoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Projetos
+        fields = "__all__"
+
+
+class AlocacoeSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = models.Alocacoes
+        fields = "__all__"
+
+
+class TecnologiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tecnologias
+        fields = "__all__"
