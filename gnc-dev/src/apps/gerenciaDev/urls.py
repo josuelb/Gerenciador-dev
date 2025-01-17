017510f668553dc58025e4b5a27b60c0dbba7ed3
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-16 22:53:02 -0300

    Update Urls do app

--- gnc-dev/src/apps/gerenciaDev/urls.py
+++ gnc-dev/src/apps/gerenciaDev/urls.py
@@ -1,16 +1,14 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from views import (
+from .views import (
     AlocacoesView,
     ProgramadorView,
     ProjetoView,
     TecnologiasView
 )
 
-
-urlpatterns = [
-    path('aloca/', AlocacoesView.as_view(), name="Alocações"),
-    path('program/', ProgramadorView.as_view(), name="Programador"),
-    path('projetos/', ProjetoView.as_view(), name="Projetos"),
-    path('Tecno/', TecnologiasView.as_view(), name="Tecnologias")
-]
+router = DefaultRouter()
+router.register(prefix=r'aloca', viewset=AlocacoesView, basename='alocar')
+router.register(r'programa', viewset=ProgramadorView, basename='programador')
+router.register(r'projetos', viewset=ProjetoView, basename='projetos')
+router.register(r'tecno', viewset=TecnologiasView, basename='tecnologias')
