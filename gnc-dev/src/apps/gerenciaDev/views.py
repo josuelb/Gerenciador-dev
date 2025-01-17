fbdb7bafd044ef1a8f919cb0f727f6840ad702ee
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-17 15:48:46 -0300

    metodo deletar

--- gnc-dev/src/apps/gerenciaDev/views.py
+++ gnc-dev/src/apps/gerenciaDev/views.py
@@ -12,11 +12,55 @@ class ProgramadorView(viewsets.ModelViewSet):
     serializer_class = serializer.ProgramadorSerializer
     queryset = models.Programador.objects.all()
 
+    @action(detail=False, methods=['delete'])
+    def apagarProgramador(self, request):
+        id_programaddor = request.data.get('id')
+        if not id_programaddor:
+            return Response(
+                {'status': 'Não foi passado o id'},
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+        try:
+            programador = models.Programador.objects.get(id=id_programaddor) 
+            programador.delete()
+            
+            return Response(
+                {'status': 'O programador foi deletado'},
+                status=HTTPStatus.OK
+            )
+        except models.Programador.DoesNotExist as error:
+            return Response(
+                {'status': 'O programador não encontrado'},
+                status=error
+            )
+
 
 class ProjetoView(viewsets.ModelViewSet):
     serializer_class = serializer.ProjetoSerializer
     queryset = models.Projetos.objects.all()
 
+    @action(detail=False, methods=['delete'])
+    def apagarProjeto(self, request):
+        id_current = request.data.get('id')
+        if not id_current:
+            return Response(
+                {'status': 'O Id não foi passado!'},
+                status=HTTPStatus.BAD_REQUEST
+            )
+        
+        try:
+            projetos_db = models.Projetos.objects.get(id=id_current)
+            projetos_db.delete()
+            return Response(
+                {'status': 'Projeto deletado'},
+                status=HTTPStatus.OK
+            )
+        except models.Projetos.DoesNotExist as error:
+            return Response(
+                {'status': 'O projeto não está cadastrado!'},
+                status=error
+            )
 
 class AlocacoesView(viewsets.ModelViewSet):
     serializer_class = serializer.AlocacoeSerializer
@@ -72,17 +116,62 @@ class AlocacoesView(viewsets.ModelViewSet):
                     {'status': 'Programador já alocado neste Projeto!'},
                     status=HTTPStatus.ACCEPTED
                 )
-        except models.Programador.DoesNotExist: 
+        except models.Programador.DoesNotExist as error: 
             return Response(
                 {'error': 'Programador não encontrado'}, 
-                status=HTTPStatus.NOT_FOUND)
-        except models.Programador.DoesNotExist: 
+                status=error
+            )
+        except models.Projetos.DoesNotExist as error: 
             return Response(
                 {'error': 'Projeto não encontrado'}, 
-                status=HTTPStatus.NOT_FOUND)
+                status=error
+            )
+        
+    @action(detail=True, methods=['delete'])
+    def apagarAlocacao(self, request, pk=None):
+        if not request.data['id']:
+            return Response(
+                {'status': 'Não foi passado o id'},
+                status=HTTPStatus.BAD_REQUEST
+            )
+        alocacao_db = models.Alocacoes.objects.get(id=request.data['id'])
+
+        try:
+            alocacao_db.delete()
+            return Response(
+                {'status':'Alocação deletada!'},
+                status=HTTPStatus.OK
+            )
+        except models.Alocacoes.DoesNotExist as error:
+            return Response(
+                {'status': 'Alocação não existente'},
+                status=error
+            )
 
 
 class TecnologiasView(viewsets.ModelViewSet):
     serializer_class = serializer.TecnologiaSerializer
     queryset = models.Tecnologias.objects.all()
+
+    @action(detail=False, methods=['delete'])
+    def apagarTecnologia(self, request):
+        id_current = request.data.get('id')
+        if not id_current:
+            return Response(
+                {'status': 'O Id da tecnologia não foi passado'},
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+        try: 
+            tecnologia_db = models.Tecnologias.objects.get(id=id_current)
+            tecnologia_db.delete()
+            return Response(
+                {'status': 'Tecnologia deletada'},
+                status=HTTPStatus.OK
+            )
+        except models.Tecnologias.DoesNotExist as error:
+            return Response(
+                {'status': 'Tecnologia não encontrada ou nao existente'},
+                status=error
+            )
 
