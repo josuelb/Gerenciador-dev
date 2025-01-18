f6cbc143e488cb42c7310c08252fdba140d8fc25
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-17 22:56:54 -0300

    Criação do user de authenticacao

--- gnc-dev/src/apps/gerenciaDev/models.py
+++ gnc-dev/src/apps/gerenciaDev/models.py
@@ -1,6 +1,6 @@
+import uuid
 from django.db import models
 from django.utils import timezone
-import uuid
 
 
 class Tecnologias(models.Model):
--- gnc-dev/src/apps/gerenciaDev/serializer.py
+++ gnc-dev/src/apps/gerenciaDev/serializer.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from . import models
 
+class UserSerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = User 
+        fields = ['username', 'password', 'email'] 
+        def create(self, validated_data): 
+            user = User.objects.create_user( 
+                username=validated_data['username'], 
+                email=validated_data['email'], 
+                password=validated_data['password'] 
+            ) 
+            return user
+    
 
 class ProgramadorSerializer(serializers.ModelSerializer):
     class Meta:
