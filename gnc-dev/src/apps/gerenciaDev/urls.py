0022d9bbdf9bcde673244dce525e3bdc5bd8088b
Josué Luiz Barbosa e Silva <104951932+josuelb@users.noreply.github.com>
2025-01-17 22:56:13 -0300

    Update urls.py

--- gnc-dev/src/apps/gerenciaDev/urls.py
+++ gnc-dev/src/apps/gerenciaDev/urls.py
@@ -4,11 +4,13 @@ from .views import (
     AlocacoesView,
     ProgramadorView,
     ProjetoView,
-    TecnologiasView
+    TecnologiasView,
+    UserCreateView
 )
 
 router = DefaultRouter()
-router.register(prefix=r'aloca', viewset=AlocacoesView, basename='alocar')
+router.register(prefix=r'alocacoes', viewset=AlocacoesView, basename='alocar')
 router.register(r'programa', viewset=ProgramadorView, basename='programador')
 router.register(r'projetos', viewset=ProjetoView, basename='projetos')
 router.register(r'tecno', viewset=TecnologiasView, basename='tecnologias')
+router.register(r'register', viewset=UserCreateView, basename='Registrar')
